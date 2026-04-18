69132b9b066776a61e5ce55b2bacfe5c2ca3de64
Chasty23 <stivencubiasc84@gmail.com>
2026-04-18 12:05:50 -0600

    feat: agregacion de las rutas del modulo rrhh

--- app/rrhh/urls.py
+++ app/rrhh/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('personal/', views.lista_empleados, name='lista_empleados'),
+    path('personal/detalle/<int:empleado_id>/', views.detalle_empleado, name='detalle_empleado'),
 ]

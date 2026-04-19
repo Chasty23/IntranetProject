9750dd9ebdcbb7501a3d7126bf1e0add4e57ea66
lupxg <gerson.chachagua80@gmail.com>
2026-04-19 09:18:28 -0600

    feat: add logout

--- app/src/urls.py
+++ app/src/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView, TemplateView
+from django.contrib.auth import views as auth_views # Import this!
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +11,6 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('rrhh/', include('rrhh.urls')),
     path('finanzas/', include('finanzas.urls')),
-    # ESTA URL ES DE PRUEBA , SIMULA EL HOME /
     path('', TemplateView.as_view(template_name='index.html')),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]

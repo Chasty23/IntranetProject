e0bde6f8e757e731b1005849f3400158bceb4854
Chasty23 <stivencubiasc84@gmail.com>
2026-04-23 14:50:30 -0600

    feat: resolucion de rutas

--- app/src/settings.py
+++ app/src/settings.py
@@ -105,9 +105,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "src/static",
+    BASE_DIR / "src" / "static",
 ]
 # para que al iniciar sesion, me mande al módulo academico
 LOGIN_REDIRECT_URL = '/index/'
